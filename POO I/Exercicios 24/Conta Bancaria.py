d5eafa1630484abff8ba00bfefe30260d7de3594
thiago <thiago.comelli@outlook.com>
2019-06-18 17:52:36 -0300

    Exercicios

--- POO I/Exercicios 24/Conta Bancaria.py
+++ POO I/Exercicios 24/Conta Bancaria.py
@@ -9,10 +9,14 @@ class Conta():
         self.__cliente.addconta(self)
         self.__gerente.addconta(self)
 
+    def getCliente(self):
+        return self.__cliente
+    def getGerente(self):
+        return self.__gerente
+
     def sacar(self,x):
         if self.__saldo - x >= 0:
             self.__saldo -= x
-
         else:
             return 'saldo insuficiente'
     def depositar(self,x):
@@ -21,7 +25,9 @@ class Conta():
         return 'valor invalido'
     def saldo(self):
         return self.__saldo
-
+    def __repr__(self):
+        return 'Agencia: {}\nConta numero: {}\nCliente: {}\nGerente: {}\nSaldo: {}\n'\
+            .format(self.__agencia,self.__numero,self.getCliente(),self.getGerente(),self.saldo())
 
 class Pessoa():
     def __init__(self,nome,anonasci):
@@ -48,32 +54,42 @@ class Cliente(Pessoa):
         for i in self.contas():
             tot += i.saldo()
         return tot
+    def getNomecliente(self):
+        return self.__nome
 
 class Gerente(Pessoa):
     def __init__(self,nome,registro,anonasci):
         super().__init__(nome,anonasci)
         self.__registro = registro
+    '''
     def gerenteadm(self):
         tot = 0
         for i in self.contas():
             tot += i.saldo
         return tot
+    '''
+    def getNomeGerente(self):
+        return self.__nome
 
 c0 = Cliente('Thiago C.','28/04/2000','SC')
 c1 = Cliente('Thiago Comelli.','28/04/2000','SC')
+
 g0 = Gerente('BomDia','19','01/01/1909')
+
 conta0 = Conta('QWE','QWER',c0,g0)
 conta1 = Conta('ASD','ASDF',c1,g0)
 conta2 = Conta('QZX','ZXCV',c1,g0)
 
 print('Thiago C. possui %s contas '%c0.qtdecontas())
 print('Thiago Comelli. possui %s contas '%c1.qtdecontas())
+print('BomDia cuida de %s contas'%g0.qtdecontas())
+print()
 conta0.depositar(100)
 conta1.depositar(10000)
 conta2.depositar(1000)
 conta1.sacar(11000)
 conta1.sacar(1000)
-print(conta0.saldo())
-print(conta1.saldo())
-print(conta2.saldo())
-print(c1.dinheiroTotal())
+print(conta0)
+print(conta1)
+print(conta2)
+#print(c1.dinheiroTotal())
