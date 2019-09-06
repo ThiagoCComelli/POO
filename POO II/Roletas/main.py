a21c8867646ae18159e46fa2a3d08eba869734eb
thiago <thiago.comelli@outlook.com>
2019-09-06 11:34:21 -0300

    Exercicios

--- POO II/Roletas/main.py
+++ POO II/Roletas/main.py
@@ -10,6 +10,7 @@ class A:
     def __init__(self, master):
         self.roleta = Roleta()
         self.players = 0
+        self.vez = 0
         self.playersjogando = []
         self.modo = "NENHUM"
         self.banco = 1000
@@ -17,6 +18,7 @@ class A:
         self.menu()
 
     def menu(self):
+
         self.photo = PhotoImage(file="preto.gif")
         self.backlabel = Label(root, image=self.photo).place(x=-1, y=-1)
 
@@ -60,50 +62,50 @@ class A:
 
     def apostas(self):
 
-        ap3 = tkinter.Button(master=root,text="3",width=1,height=0,fg="red",bg="yellow",font="Times 13 bold").place(x=568,y=462)
-        ap6 = tkinter.Button(master=root,text="6",width=1,height=0,fg="red",bg="yellow",font="Times 13 bold").place(x=663,y=462)
-        ap9 = tkinter.Button(master=root,text="9",width=1,height=0,fg="red",bg="yellow",font="Times 13 bold").place(x=757,y=462)
-        ap12 = tkinter.Button(master=root,text="12",width=1,height=0,fg="red",bg="yellow",font="Times 13 bold").place(x=855,y=462)
-        ap15 = tkinter.Button(master=root,text="15",width=1,height=0,fg="red",bg="yellow",font="Times 13 bold").place(x=950,y=462)
-        ap18 = tkinter.Button(master=root,text="18",width=1,height=0,fg="red",bg="yellow",font="Times 13 bold").place(x=1051,y=462)
-        ap21 = tkinter.Button(master=root,text="21",width=1,height=0,fg="red",bg="yellow",font="Times 13 bold").place(x=1145,y=462)
-        ap24 = tkinter.Button(master=root,text="24",width=1,height=0,fg="red",bg="yellow",font="Times 13 bold").place(x=1242,y=462)
-        ap27 = tkinter.Button(master=root,text="27",width=1,height=0,fg="red",bg="yellow",font="Times 13 bold").place(x=1340,y=462)
-        ap30 = tkinter.Button(master=root,text="30",width=1,height=0,fg="red",bg="yellow",font="Times 13 bold").place(x=1436,y=462)
-        ap33 = tkinter.Button(master=root,text="33",width=1,height=0,fg="red",bg="yellow",font="Times 13 bold").place(x=1533,y=462)
-        ap36 = tkinter.Button(master=root,text="36",width=1,height=0,fg="red",bg="yellow",font="Times 13 bold").place(x=1630,y=462)
+        ap3 = tkinter.Button(master=root,text="3",width=1,height=0,fg="red",bg="yellow",font="Times 13 bold",command=lambda:self.fazerApostaBotoes(3)).place(x=568,y=462)
+        ap6 = tkinter.Button(master=root,text="6",width=1,height=0,fg="red",bg="yellow",font="Times 13 bold",command=lambda:self.fazerApostaBotoes(6)).place(x=663,y=462)
+        ap9 = tkinter.Button(master=root,text="9",width=1,height=0,fg="red",bg="yellow",font="Times 13 bold",command=lambda:self.fazerApostaBotoes(9)).place(x=757,y=462)
+        ap12 = tkinter.Button(master=root,text="12",width=1,height=0,fg="red",bg="yellow",font="Times 13 bold",command=lambda:self.fazerApostaBotoes(12)).place(x=855,y=462)
+        ap15 = tkinter.Button(master=root,text="15",width=1,height=0,fg="red",bg="yellow",font="Times 13 bold",command=lambda:self.fazerApostaBotoes(15)).place(x=950,y=462)
+        ap18 = tkinter.Button(master=root,text="18",width=1,height=0,fg="red",bg="yellow",font="Times 13 bold",command=lambda:self.fazerApostaBotoes(18)).place(x=1051,y=462)
+        ap21 = tkinter.Button(master=root,text="21",width=1,height=0,fg="red",bg="yellow",font="Times 13 bold",command=lambda:self.fazerApostaBotoes(21)).place(x=1145,y=462)
+        ap24 = tkinter.Button(master=root,text="24",width=1,height=0,fg="red",bg="yellow",font="Times 13 bold",command=lambda:self.fazerApostaBotoes(24)).place(x=1242,y=462)
+        ap27 = tkinter.Button(master=root,text="27",width=1,height=0,fg="red",bg="yellow",font="Times 13 bold",command=lambda:self.fazerApostaBotoes(27)).place(x=1340,y=462)
+        ap30 = tkinter.Button(master=root,text="30",width=1,height=0,fg="red",bg="yellow",font="Times 13 bold",command=lambda:self.fazerApostaBotoes(30)).place(x=1436,y=462)
+        ap33 = tkinter.Button(master=root,text="33",width=1,height=0,fg="red",bg="yellow",font="Times 13 bold",command=lambda:self.fazerApostaBotoes(33)).place(x=1533,y=462)
+        ap36 = tkinter.Button(master=root,text="36",width=1,height=0,fg="red",bg="yellow",font="Times 13 bold",command=lambda:self.fazerApostaBotoes(36)).place(x=1630,y=462)
 
-        ap2 = tkinter.Button(master=root, text="2", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold").place(x=568, y=615)
-        ap5 = tkinter.Button(master=root, text="5", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold").place(x=663, y=615)
-        ap8 = tkinter.Button(master=root, text="8", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold").place(x=757, y=615)
-        ap11 = tkinter.Button(master=root, text="11", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold").place(x=855, y=615)
-        ap14 = tkinter.Button(master=root, text="14", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold").place(x=950, y=615)
-        ap17 = tkinter.Button(master=root, text="17", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold").place(x=1051, y=615)
-        ap20 = tkinter.Button(master=root, text="20", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold").place(x=1145, y=615)
-        ap23 = tkinter.Button(master=root, text="23", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold").place(x=1242, y=615)
-        ap26 = tkinter.Button(master=root, text="25", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold").place(x=1340, y=615)
-        ap29 = tkinter.Button(master=root, text="29", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold").place(x=1436, y=615)
-        ap32 = tkinter.Button(master=root, text="32", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold").place(x=1533, y=615)
-        ap35 = tkinter.Button(master=root, text="35", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold").place(x=1630, y=615)
+        ap2 = tkinter.Button(master=root, text="2", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold",command=lambda:self.fazerApostaBotoes(2)).place(x=568, y=615)
+        ap5 = tkinter.Button(master=root, text="5", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold",command=lambda:self.fazerApostaBotoes(5)).place(x=663, y=615)
+        ap8 = tkinter.Button(master=root, text="8", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold",command=lambda:self.fazerApostaBotoes(8)).place(x=757, y=615)
+        ap11 = tkinter.Button(master=root, text="11", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold",command=lambda:self.fazerApostaBotoes(11)).place(x=855, y=615)
+        ap14 = tkinter.Button(master=root, text="14", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold",command=lambda:self.fazerApostaBotoes(14)).place(x=950, y=615)
+        ap17 = tkinter.Button(master=root, text="17", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold",command=lambda:self.fazerApostaBotoes(17)).place(x=1051, y=615)
+        ap20 = tkinter.Button(master=root, text="20", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold",command=lambda:self.fazerApostaBotoes(20)).place(x=1145, y=615)
+        ap23 = tkinter.Button(master=root, text="23", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold",command=lambda:self.fazerApostaBotoes(23)).place(x=1242, y=615)
+        ap26 = tkinter.Button(master=root, text="25", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold",command=lambda:self.fazerApostaBotoes(26)).place(x=1340, y=615)
+        ap29 = tkinter.Button(master=root, text="29", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold",command=lambda:self.fazerApostaBotoes(29)).place(x=1436, y=615)
+        ap32 = tkinter.Button(master=root, text="32", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold",command=lambda:self.fazerApostaBotoes(32)).place(x=1533, y=615)
+        ap35 = tkinter.Button(master=root, text="35", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold",command=lambda:self.fazerApostaBotoes(35)).place(x=1630, y=615)
 
-        ap1 = tkinter.Button(master=root, text="1", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold").place(x=568, y=769)
-        ap4 = tkinter.Button(master=root, text="4", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold").place(x=663, y=769)
-        ap7 = tkinter.Button(master=root, text="7", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold").place(x=757, y=769)
-        ap10 = tkinter.Button(master=root, text="10", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold").place(x=855, y=769)
-        ap13 = tkinter.Button(master=root, text="13", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold").place(x=950, y=769)
-        ap16 = tkinter.Button(master=root, text="16", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold").place(x=1051, y=769)
-        ap19 = tkinter.Button(master=root, text="19", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold").place(x=1145, y=769)
-        ap22 = tkinter.Button(master=root, text="22", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold").place(x=1242, y=769)
-        ap25 = tkinter.Button(master=root, text="25", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold").place(x=1340, y=769)
-        ap28 = tkinter.Button(master=root, text="28", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold").place(x=1436, y=769)
-        ap31 = tkinter.Button(master=root, text="31", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold").place(x=1533, y=769)
-        ap34 = tkinter.Button(master=root, text="34", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold").place(x=1630, y=769)
+        ap1 = tkinter.Button(master=root, text="1", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold",command=lambda:self.fazerApostaBotoes(1)).place(x=568, y=769)
+        ap4 = tkinter.Button(master=root, text="4", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold",command=lambda:self.fazerApostaBotoes(4)).place(x=663, y=769)
+        ap7 = tkinter.Button(master=root, text="7", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold",command=lambda:self.fazerApostaBotoes(7)).place(x=757, y=769)
+        ap10 = tkinter.Button(master=root, text="10", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold",command=lambda:self.fazerApostaBotoes(10)).place(x=855, y=769)
+        ap13 = tkinter.Button(master=root, text="13", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold",command=lambda:self.fazerApostaBotoes(13)).place(x=950, y=769)
+        ap16 = tkinter.Button(master=root, text="16", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold",command=lambda:self.fazerApostaBotoes(16)).place(x=1051, y=769)
+        ap19 = tkinter.Button(master=root, text="19", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold",command=lambda:self.fazerApostaBotoes(19)).place(x=1145, y=769)
+        ap22 = tkinter.Button(master=root, text="22", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold",command=lambda:self.fazerApostaBotoes(22)).place(x=1242, y=769)
+        ap25 = tkinter.Button(master=root, text="25", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold",command=lambda:self.fazerApostaBotoes(25)).place(x=1340, y=769)
+        ap28 = tkinter.Button(master=root, text="28", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold",command=lambda:self.fazerApostaBotoes(28)).place(x=1436, y=769)
+        ap31 = tkinter.Button(master=root, text="31", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold",command=lambda:self.fazerApostaBotoes(31)).place(x=1533, y=769)
+        ap34 = tkinter.Button(master=root, text="34", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold",command=lambda:self.fazerApostaBotoes(34)).place(x=1630, y=769)
 
         linha1 = tkinter.Button(master=root, text="", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold").place(x=1727, y=462)
         linha1 = tkinter.Button(master=root, text="", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold").place(x=1727, y=615)
         linha1 = tkinter.Button(master=root, text="", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold").place(x=1727, y=769)
 
-        zero = tkinter.Button(master=root, text="0", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold").place(x=471, y=615)
+        zero = tkinter.Button(master=root, text="0", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold",command=lambda:self.fazerApostaBotoes(0)).place(x=471, y=615)
         ap1to12 = tkinter.Button(master=root, text="", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold").place(x=710, y=830)
         ap2to12 = tkinter.Button(master=root, text="", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold").place(x=1100, y=830)
         ap3to12 = tkinter.Button(master=root, text="", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold").place(x=1490, y=830)
@@ -116,20 +118,20 @@ class A:
         dezenoveto36 = tkinter.Button(master=root, text="", width=1, height=0, fg="red", bg="yellow",font="Times 13 bold").place(x=1579, y=900)
 
 
-        um = tkinter.Button(master=root, text="1", width=5, height=0, fg="red", bg="black",font="Times 13 bold").place(x=50, y=750)
-        dois = tkinter.Button(master=root, text="2", width=5, height=0, fg="red", bg="black",font="Times 13 bold").place(x=130, y=750)
-        tres = tkinter.Button(master=root, text="5", width=5, height=0, fg="red", bg="black",font="Times 13 bold").place(x=210, y=750)
-        quatro = tkinter.Button(master=root, text="10", width=5, height=0, fg="red", bg="black",font="Times 13 bold").place(x=50, y=790)
-        cinco = tkinter.Button(master=root, text="20", width=5, height=0, fg="red", bg="black",font="Times 13 bold").place(x=130, y=790)
-        seis = tkinter.Button(master=root, text="25", width=5, height=0, fg="red", bg="black",font="Times 13 bold").place(x=210, y=790)
-        sete = tkinter.Button(master=root, text="50", width=5, height=0, fg="red", bg="black",font="Times 13 bold").place(x=50, y=830)
-        oito = tkinter.Button(master=root, text="75", width=5, height=0, fg="red", bg="black",font="Times 13 bold").place(x=130, y=830)
-        nove = tkinter.Button(master=root, text="100", width=5, height=0, fg="red", bg="black",font="Times 13 bold").place(x=210, y=830)
-        zerar = tkinter.Button(master=root, text="ZERAR", width=5, height=0, fg="red", bg="black",font="Times 13 bold").place(x=130, y=870)
+        um = tkinter.Button(master=root, text="1", width=5, height=0, fg="red", bg="black",font="Times 13 bold",command=lambda:self.fazerAposta(1)).place(x=50, y=750)
+        dois = tkinter.Button(master=root, text="2", width=5, height=0, fg="red", bg="black",font="Times 13 bold",command=lambda:self.fazerAposta(2)).place(x=130, y=750)
+        tres = tkinter.Button(master=root, text="5", width=5, height=0, fg="red", bg="black",font="Times 13 bold",command=lambda:self.fazerAposta(5)).place(x=210, y=750)
+        quatro = tkinter.Button(master=root, text="10", width=5, height=0, fg="red", bg="black",font="Times 13 bold",command=lambda:self.fazerAposta(10)).place(x=50, y=790)
+        cinco = tkinter.Button(master=root, text="20", width=5, height=0, fg="red", bg="black",font="Times 13 bold",command=lambda:self.fazerAposta(20)).place(x=130, y=790)
+        seis = tkinter.Button(master=root, text="25", width=5, height=0, fg="red", bg="black",font="Times 13 bold",command=lambda:self.fazerAposta(25)).place(x=210, y=790)
+        sete = tkinter.Button(master=root, text="50", width=5, height=0, fg="red", bg="black",font="Times 13 bold",command=lambda:self.fazerAposta(50)).place(x=50, y=830)
+        oito = tkinter.Button(master=root, text="75", width=5, height=0, fg="red", bg="black",font="Times 13 bold",command=lambda:self.fazerAposta(75)).place(x=130, y=830)
+        nove = tkinter.Button(master=root, text="100", width=5, height=0, fg="red", bg="black",font="Times 13 bold",command=lambda:self.fazerAposta(100)).place(x=210, y=830)
+        zerar = tkinter.Button(master=root, text="ZERAR", width=5, height=0, fg="red", bg="black",font="Times 13 bold",command=lambda:self.fazerAposta(0)).place(x=130, y=870)
 
-        self.playeraposta = tk.Label()
-        self.playeraposta.grid(row=0, column=0)
-        self.playeraposta.place(x=100, y=100)
+        apostar = tkinter.Button(master=root, text="APOSTAR", width=5, height=0, fg="red", bg="black",font="Times 13 bold").place(x=130, y=930)
+
+
 
     def voltarMenu(self):
         if self.getPl() == 0:
@@ -158,6 +160,12 @@ class A:
     def getPl(self):
         return self.players
 
+    def getVez(self):
+        return self.vez
+
+    def setVez(self):
+        self.vez += 1
+
     def setPlayers(self):
         nome = ["ALPHA","BETA","CHARLIE","DELTA"]
         for i in range(self.players):
@@ -170,6 +178,8 @@ class A:
             self.photo = PhotoImage(file="tabela.gif")
             self.backlabel = Label(root, image=self.photo).place(x=-1,y=-1)
 
+            self.apostas()
+
             self.titulo = tk.Label()
             self.titulo.grid(row=0, column=0)
             self.titulo.configure(text='EUROPEU', font="Times 25 bold",fg="red",bg="black")
@@ -177,8 +187,37 @@ class A:
 
             voltar = tkinter.Button(master=root,text="voltar para o menu",width=15,command=self.voltarMenu,fg="red",bg="black",font=("Times 13 bold")).place(x=10,y=10)
 
-            self.apostas()
+            self.valornatela = tk.Label()
+            self.valornatela.grid(row=0, column=0)
+            self.valornatela.configure(text='{}'.format(self.playersjogando[self.getVez()].getAposta()), font="Times 25 bold",fg="red", bg="black")
+            self.valornatela.place(x=50, y=927)
 
+            self.jogadordavez = tk.Label()
+            self.jogadordavez.grid(row=0, column=0)
+            self.jogadordavez.configure(text='{}'.format(self.playersjogando[self.getVez()].getNome()), font="Times 25 bold",fg="red", bg="black")
+            self.jogadordavez.place(x=210, y=927)
+
+    def fazerAposta(self,valor):
+        if valor == 0:
+            self.playersjogando[self.getVez()].setApostaZerar()
+        else:
+            self.playersjogando[self.getVez()].setAposta(valor)
+        self.valornatela.configure(text='{}'.format(self.playersjogando[self.getVez()].getAposta()),font="Times 25 bold", fg="red", bg="black")
+
+    def fazerApostaBotoes(self,numero):
+        self.playersjogando[self.getVez()].setJogadas(numero)
+        print(self.playersjogando[self.getVez()].getJogadas())
+
+
+    def loopingJogada(self):
+        for i in range(self.players):
+            self.valornatela.configure(text='{}'.format(self.playersjogando[i].getTip()), font="Times 25 bold",fg="red", bg="black")
+            self.jogadordavez.configure(text='asd {}'.format(self.playersjogando[i].getNome()), font="Times 25 bold",fg="red", bg="black")
+
+            if self.playersjogando[i].getProsseguir() != True:
+                while True:
+                    if self.playersjogando[i].getProsseguir() == True:
+                        break
 
 
     def americano(self):
--- POO II/Roletas/players.py
+++ POO II/Roletas/players.py
@@ -5,16 +5,34 @@ from tkinter import *
 from random import randint,shuffle
 import random
 
+
 class Player():
     def __init__(self,nome):
         self.__nome = nome
         self.__tips = 100
+        self.__jogadas = []
+        self.__aposta = 0
+        self.__prosseguir = False
 
     def getNome(self):
         return self.__nome
     def getTip(self):
         return self.__tips
+    def getJogadas(self):
+        return self.__jogadas
+    def getAposta(self):
+        return self.__aposta
+    def getProsseguir(self):
+        return self.__prosseguir
 
 
     def setTip(self,value):
         self.__tips += value
+    def setAposta(self,value):
+        self.__aposta += value
+    def setApostaZerar(self):
+        self.__aposta = 0
+    def setJogadas(self,jogada):
+        self.__jogadas.append(jogada)
+    def setProsseguir(self,condicao):
+        self.__prosseguir = condicao
